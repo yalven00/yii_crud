5416a14db0f15b2c2be3116b5a653fdf74be3a6b
yalven00 <127588837+yalven00@users.noreply.github.com>
2025-06-07 22:01:29 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=db_track',
     'username' => 'root',
-    'password' => 'Ghfdbkj,jz1990ujl!',
+    'password' => 'dbpwd',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
